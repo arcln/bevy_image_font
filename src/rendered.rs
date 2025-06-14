983839d6ff17e251eb1a497cb19c6dd5e8c5b2b2
Arthur Chaloin <arthur.chaloin@gmail.com>
2025-06-14 20:20:57 +0200

    fix: remove the need to have space character in set

--- src/rendered.rs
+++ src/rendered.rs
@@ -379,7 +379,7 @@ fn render_glyphs_to_image(
     font_textures: Vec<ImageBuffer<Rgba<u8>, &[u8]>>,
 ) -> Result<(), ImageFontRenderError> {
     let mut x_pos = 0.0;
-    let mut texture_atlas = render_context.font_texture_atlas(' ');
+    let mut texture_atlas = render_context.font_texture_atlas('\0');
     let mut color = Color::default();
 
     for (character, next_character) in render_context
